fb65b19d66eebfa6e742119cfb5daccddf3cbade
ula345 <krzeminska.ula@gmail.com>
2021-09-25 15:31:32 +0200

    Fixed verson 1

--- Src/app.js
+++ Src/app.js
@@ -1,6 +1,37 @@
-function getForecast(coordinates) {
-console.log(response)
-let apiKey="9ab632720a8f655a49d407e0a291ff8b"; 
-let apiUrl='https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric';
-axios.get(apiUrl).then(displayTemperature)
+function showTemperature(response) {
+    let temperature = math.round(response.data.main.temp); 
+    let heading = document.querySelector("h1");
+    heading.innerHTML = '${temperature}';
 }
+
+function retrievePosition(coordinates) {
+let latitude = coordinates.coords.latitude; 
+let longitude = coordinates.coords.longitude; 
+let apiKey="9ab632720a8f655a49d407e0a291ff8b"; 
+let apiUrl='https://api.openweathermap.org/data/2.5/data?lat=${coordinates.latitude}&lon=${coordinates.longitude}&appid=${apiKey}&units=metric';
+
+axios.get('${apiUrl}&appid=${apiKey}').then(showTemperature); 
+
+}
+function getCurrentPosition() { 
+
+    navigator.geolocation.getCurrentPosition(retrievePosition);
+}
+
+let button = document.querySelector("button"); 
+button.addEventListener("click", getCurrentPosition);
+
+function search (event) {
+event.preventDefault();
+let searchInput = document.querySelector("#search-text-input");
+console.log(searchInput.value);
+let h1 = document.querySelector("h1");
+h1.innerHTML ='${searchInput.value}';
+}
+
+
+let form = document.querySelector("#search-form");
+
+form.addEventListener("submit", search);
+
+
