89e3c5b284180c679b3a5e0562709c92eb531bdd
ula345 <krzeminska.ula@gmail.com>
2021-09-25 20:20:44 +0200

    form

--- Src/app.js
+++ Src/app.js
@@ -43,12 +43,23 @@ descriptionElement.innerHTML = response.data.weather[0].description;
 humidityElement.innerHTML = response.data.main.humidity;
 windElement.innerHTML= Math.round(response.data.wind.speed);
 dateElement.innerHTML= formatDate(response.cdata.dt*1000);
-iconElement.setAttriburte("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
-iconElement.setAttriburte("alt", response.data.weather[0].description);
+iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "33584fd56cf782d965dc41af18326ed8";
-let city = "Lisbon";
+function search(city) {
+    let apiKey = "33584fd56cf782d965dc41af18326ed8";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
 axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+    event.preventDefault(); 
+    let cityInputElement = document. querySelector("#city-input");
+    search(cityInputElement.value);
+}
+
+
+
+let form= document.querySelector("#search-form");
+form.addEventListener("submit",handleSubmit);
